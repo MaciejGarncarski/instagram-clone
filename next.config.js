cdf0c23c420de0ab149d79b244aab671d8f159c4
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-09-08 16:30:38 +0200

    ci: fix svg error

--- next.config.js
+++ next.config.js
@@ -20,4 +20,21 @@ module.exports = withPWA({
   images: {
     domains: [`${process.env.NEXT_PUBLIC_SUPABASE_URL_START}.supabase.co`],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 });
