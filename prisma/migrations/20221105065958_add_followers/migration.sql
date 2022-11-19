3ded1f75e51b0a319536b5f74eec6ae7d9312f15
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-11-19 19:50:14 +0100

    fix: infinite pending getProfileByUsername request

--- prisma/migrations/20221105065958_add_followers/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "followers" (
-    "id" SERIAL NOT NULL,
-    "from_user_id" UUID NOT NULL,
-    "to_user_id" UUID NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "followers_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20221117141402_remove_post_uuid/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `img_uuid` on the `posts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "posts" DROP COLUMN "img_uuid";
